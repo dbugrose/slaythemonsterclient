37c6ab982cab5a6371ce4a909c883498ea405bbd
dbugrose <darcy.ophelia.rose@gmail.com>
2026-02-25 15:44:12 -0800

    text color update

--- app/components/MissionBoard.tsx
+++ app/components/MissionBoard.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MissionBoard = () => {
   return (
-    <div className="w-[clamp(100px,80vw,700px)] min-h-screen p-10 bg-[url(/assets/11468999.png)] bg-size-[100%_100%] flex flex-col items-center justify-center">
+    <div className="w-[clamp(100px,80vw,700px)] min-h-screen p-10 bg-[url(/assets/11468999.png)] text-[#593819] bg-size-[100%_100%] flex flex-col items-center justify-center">
       <h1>Chore 1</h1>
       <h1>Chore 2</h1>
       <h1>Chore 3</h1>
